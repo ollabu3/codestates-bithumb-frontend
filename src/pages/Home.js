ec26ffd468401567e3e9ad9227361ffc3fe2e6a0
ollabu3 <ollabu3@gmail.com>
2022-04-01 17:23:54 +0900

    웹소켓 orderbook test, 상태관리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { SubscribeType, Symbols, TickerTypes } from "../types/type";
+import { SubscribeType, Symbols, TickerTypes, OrderType } from "../types/type";
 
 import Chart from "../components/Chart";
 import Transaction from "../components/Transaction";
@@ -19,16 +20,21 @@ const FlexRow = styled.div`
 
 const Home = () => {
   let ws = useRef(null);
+  const navigate = useNavigate();
+  const params = useParams();
+  const id = params.id ? params.id : Symbols.BTC_KRW;
+
   const [isConnected, setIsConnected] = useState(false);
   const [ticker, setTicker] = useState();
-  const [transaction, setTransaction] = useState({});
-  const [orderBook, setOrderBook] = useState({});
+  const [transaction, setTransaction] = useState([]);
+  const [orderBook, setOrderBook] = useState([]);
+
+  // {"symbol" : "BTC_KRW", "orderType" : "ask", "price" : "10596000", "quantity" : "0.5495", "total" : "8"},
   const createWebSocket = async () => {
     try {
       ws.current = new WebSocket("wss://pubwss.bithumb.com/pub/ws");
       ws.current.onopen = (e) => {
         setIsConnected((isConnected) => !isConnected);
-        console.log("onopen ==== ", e);
       };
     } catch (err) {
       setIsConnected(false);
@@ -43,27 +49,6 @@ const Home = () => {
     }));
   };
 
-  /*
-    {
-	"type" : "transaction",
-	"content" : {
-		"list" : [
-			{
-				"symbol" : "BTC_KRW",					// 통화코드
-				"buySellGb" : "1",							// 체결종류(1:매도체결, 2:매수체결)
-				"contPrice" : "10579000",					// 체결가격
-				"contQty" : "0.01",							// 체결수량
-				"contAmt" : "105790.00",					// 체결금액
-				"contDtm" : "2020-01-29 12:24:18.830039",	// 체결시각
-				"updn" : "dn"								// 직전 시세와 비교 : up-상승, dn-하락
-			}
-		]
-	}
-}
-
-BTC_KRW : [],
-ETC_KRW: [],
-    */
   const getTransactionData = (data) => {
     const symbol = data.list[0].symbol;
     const dataList = data.list.reverse();
@@ -75,38 +60,65 @@ ETC_KRW: [],
           }
         : { ...prev, [symbol]: [...dataList] }
     );
-
-    console.log(
-      "==============================================================================================================================="
-    );
   };
 
   /*
-  {
-	"type" : "orderbookdepth",
-		"content" : {
-		"list" : [
-			{
+List: [	{
 				"symbol" : "BTC_KRW",
-				"orderType" : "ask",		// 주문타입 – bid(매수가격) / ask(매도가격) 
+				"orderType" : "ask",		// 주문타입 – bid / ask
 				"price" : "10593000",		// 호가
 				"quantity" : "1.11223318",	// 잔량
 				"total" : "3"				// 건수
-			},
-			{"symbol" : "BTC_KRW", "orderType" : "ask", "price" : "10596000", "quantity" : "0.5495", "total" : "8"},
-			{"symbol" : "BTC_KRW", "orderType" : "ask", "price" : "10598000", "quantity" : "18.2085", "total" : "10"},
-			{"symbol" : "BTC_KRW", "orderType" : "bid", "price" : "10532000", "quantity" : "0", "total" : "0"},
-			{"symbol" : "BTC_KRW", "orderType" : "bid", "price" : "10572000", "quantity" : "2.3324", "total" : "4"},
-			{"symbol" : "BTC_KRW", "orderType" : "bid", "price" : "10571000", "quantity" : "1.469", "total" : "3"},
-			{"symbol" : "BTC_KRW", "orderType" : "bid", "price" : "10569000", "quantity" : "0.5152", "total" : "2"}
-		],
-		"datetime":1580268255864325		// 일시
-	}
-}
-  */
+			}]
+*/
+  const getOrderBookData = (data) => {
+    const symbol = data.list[0].symbol;
+    const dataList = data.list.sort(
+      (a, b) => Number(b.price) - Number(a.price)
+    );
 
-  const getOrderBookData = (data) => {};
+    const askDataList = dataList.filter(
+      (item) => item.orderType === OrderType.ask
+    );
 
+    const bidDataList = dataList.filter(
+      (item) => item.orderType === OrderType.bid
+    );
+
+    setOrderBook((prev) =>
+      prev[symbol]
+        ? {
+            ...prev,
+            [symbol]: {
+              ask: [...askDataList, ...prev[symbol].ask],
+              bid: [...bidDataList, ...prev[symbol].bid],
+            },
+          }
+        : {
+            ...prev,
+            [symbol]: {
+              ask: [...askDataList],
+              bid: [...bidDataList],
+            },
+          }
+    );
+    // 리스트 가지고 와서
+    /*
+    bid와 ask를 나눈 후
+    orderBook : {
+      BTC_KRW: {
+        ask : {}
+        bid : {}
+      }.
+       ETM_KRW: {
+        ask : {}
+        bid : {}
+      }.
+    }
+    */
+  };
+
+  console.log(orderBook);
   useEffect(() => {
     createWebSocket();
   }, []);
@@ -154,13 +166,14 @@ ETC_KRW: [],
       };
     }
   }, [isConnected]);
+
   return (
     <Container>
-      <SymbolList ticker={ticker} />
+      <SymbolList ticker={ticker} navigate={navigate} />
       <Chart ticker={ticker} />
       <FlexRow>
-        <Transaction ws={ws} isConnected={isConnected} />
-        <OrderBook ws={ws} isConnected={isConnected} />
+        <Transaction transaction={transaction} id={id} />
+        <OrderBook orderBook={orderBook} id={id} />
       </FlexRow>
     </Container>
   );
