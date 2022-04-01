3a8684f9f7ec4870340bd386fe6422312a5bfdab
ollabu3 <ollabu3@gmail.com>
2022-04-01 17:23:02 +0900

    coin value route 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 const App = () => {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
